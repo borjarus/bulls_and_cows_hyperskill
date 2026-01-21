4652a4a2f2c5fe5e40f89b588e8deb35e11f83a8
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2026-01-21 14:30:55 +0100

    stage 1

--- Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
+++ Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
@@ -1,5 +1,34 @@
 package bullscows
 
 fun main() {
-    println("Hello World!")
+    println("The secret code is prepared: ****.")
+
+    println("\nTurn 1. Answer:")
+    println("1234")
+    println("Grade: 1 cow.")
+
+    println("\nTurn 2. Answer:")
+    println("5678")
+    println("Grade: 1 cow.")
+
+    println("\nTurn 3. Answer:")
+    println("9012")
+    println("Grade: 1 bull and 1 cow.")
+
+    println("\nTurn 4. Answer:")
+    println("9087")
+    println("Grade: 1 bull and 1 cow.")
+
+    println("\nTurn 5. Answer:")
+    println("1087")
+    println("Grade: 1 cow.")
+
+    println("\nTurn 6. Answer:")
+    println("9205")
+    println("Grade: 3 bulls.")
+
+    println("\nTurn 7. Answer:")
+    println("9305")
+    println("Grade: 4 bulls.")
+    println("Congrats! The secret code is 9305.")
 }
